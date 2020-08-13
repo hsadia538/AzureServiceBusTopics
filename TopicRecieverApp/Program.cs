80507954b50b965043396af340246a409ad4fa8c
hsadia538 <hsadia538@gmail.com>
2020-08-13 16:46:46 +0500

    Sender and Reciever side queue

---
+++ TopicRecieverApp/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace TopicRecieverApp
+{
+    class Program
+    {
+
+        const string ServiceBusConnectionString = "Endpoint=sb://servicebuspractice.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=44Xtdc5DTF2dnGjGiRYnYAVfyPVCW3ZVM6boz8Ty/gE=";
+        const string TopicName = "practicetopic";
+        const string SubscriptionName = "sub1";
+        static ISubscriptionClient subscriptionClient;
+        public static async Task Main(string[] args)
+        {
+            subscriptionClient = new SubscriptionClient(ServiceBusConnectionString, TopicName, SubscriptionName);
+
+            
+            Console.WriteLine("Press ENTER key to exit after receiving all the messages.");
+           
+            // Register subscription message handler and receive messages in a loop
+            RegisterOnMessageHandlerAndReceiveMessages();
+
+            Console.ReadKey();
+
+            await subscriptionClient.CloseAsync();
+        }
+
+        static void RegisterOnMessageHandlerAndReceiveMessages()
+        {
+            // Configure the message handler options
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+     
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            // Register the function that processes messages.
+            subscriptionClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            // Process the message.
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+
+
+            await subscriptionClient.CompleteAsync(message.SystemProperties.LockToken);
+
+        }
+
+        // For Exception Handling
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+
+    }
+}
---
+++ TopicSenderApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+
+namespace TopicSenderApp
+{
+    class Program
+    {
+
+        const string ServiceBusConnectionString = "Endpoint=sb://servicebuspractice.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=44Xtdc5DTF2dnGjGiRYnYAVfyPVCW3ZVM6boz8Ty/gE=";
+        const string TopicName = "practicetopic";
+        static ITopicClient topicClient;
+
+        public static async Task Main(string[] args)
+        {
+            const int NumberOfMessages = 4;
+
+            topicClient = new TopicClient(ServiceBusConnectionString, TopicName);
+
+            Console.WriteLine("Press key when all messages are sent");
+
+            await SendMessageAsync(NumberOfMessages);
+            Console.ReadKey();
+
+            await topicClient.CloseAsync();
+
+        }
+
+        static async Task SendMessageAsync( int numberOfMessages)
+        {
+            try
+            {
+                for (var i = 0; i < numberOfMessages; i++)
+                {
+                    string messageBody = $"Message number {i}";
+
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    Console.WriteLine($"Sending message: {messageBody}");
+
+                    await topicClient.SendAsync(message);
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: { e.Message}");
+            }
+        }
+    }
+}
